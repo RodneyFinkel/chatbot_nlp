cd8e8a226a75482cb1d820fc0310cf883ad38e20
Rodney Finkel <111357994+RodneyFinkel@users.noreply.github.com>
2023-05-15 20:55:14 +0300

    synch

--- chatgui.py
+++ chatgui.py
@@ -106,7 +106,7 @@ SendButton = Button(base, font=("Verdana",12,'bold'), text="Send", width="12", h
                     command= send )
 
 #Create the box to enter message
-EntryBox = Text(base, bd=0, bg="white",width="29", height="5", font="Arial")
+EntryBox = Text(base, bd=0, bg="black",width="29", height="5", font="Arial")
 
 
 #Place all components on the screen
